aa2b56922a1927ac9152a275564a24f949654436
Philippe Donaus <philippe.donaus@gmail.com>
2019-06-03 21:18:18 -0400

    Working IDE With Rendering Functions

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import { render } from 'react-dom';
 import brace from 'brace';
 import AceEditor from 'react-ace';
@@ -12,12 +11,26 @@ import 'brace/theme/monokai';
 export default class Editor extends Component {
   constructor(props){
     super(props);
-    this.onChange = this.onChange.bind(this);
+    this.onChangeHTML = this.onChangeHTML.bind(this);
+    this.onChangeCSS = this.onChangeCSS.bind(this);
+    this.onChangeJS = this.onChangeJS.bind(this);
     this.state = {html:`<p>We Did it!</p>`,
                   css:`body {color:blue}`,
                   js: `function onLoad(editor){ console.log("i've loaded!!");}`}
   }
-  onChange(newValue) {
+  onChangeHTML(newValue) {
+    this.setState({html:newValue});
+    
+    console.log(newValue);
+  }
+  onChangeCSS(newValue) {
+    this.setState({css:newValue});
+    
+    console.log(newValue);
+  }
+  onChangeJS(newValue) {
+    this.setState({js:newValue});
+    
     console.log(newValue);
   }
   
@@ -26,17 +39,17 @@ export default class Editor extends Component {
       <div>
         <CodeIDE 
           mycode={this.state.html}
-          onChange={this.onChange}
+          onChange={this.onChangeHTML}
           mymode={'html'}
           />
         <CodeIDE 
           mycode={this.state.css}
-          onChange={this.onChange}
+          onChange={this.onChangeCSS}
           mymode={'css'}
           />
         <CodeIDE 
           mycode={this.state.js}
-          onChange={this.onChange}
+          onChange={this.onChangeJS}
           mymode={'javascript'}
           />
         <RenderDisplay
@@ -70,7 +83,7 @@ class CodeIDE extends Component {
               enableLiveAutocompletion: true,
               enableSnippets: false,
               showLineNumbers: true,
-              tabSize: 2,
+              tabSize: 4,
               }}/>
       </div>
 	);
@@ -80,32 +93,29 @@ class CodeIDE extends Component {
 
 class RenderDisplay extends Component {
   componentDidMount(){
-        this.compile();
-    }
-
-    compile() {
-        var code = document.getElementById("displaycase").contentWindow.document;
-      
-        document.body.onkeyup = () => {
-          code.open();
-          code.writeln(
-            this.props.htmlData +
-              "<style>" +
-              this.props.cssData +
-              "</style>" +
-              "<script>" +
-              this.props.jsData +
-              "</script>"
-          );
-          code.close();
-        };
-      }
-    render(){
-        return(
-
-            <div className="col s6">
-                <iframe id="displaycase"></iframe>
-            </div>
-    )
-    }
+    this.compile();
+  }
+  compile = () => {
+    var code = document.getElementById("displaycase").contentWindow.document;
+    document.body.onkeyup = () => {
+      code.open();
+      code.writeln(
+        this.props.htmlData +
+          `<style>` +
+          this.props.cssData +
+          `</style>` +
+          `<script>` +
+          this.props.jsData +
+          `</script>`
+      );
+      code.close();
+    };
+  }
+  render(){
+      return(
+          <div className="col s6">
+              <iframe id="displaycase"></iframe>
+          </div>
+  );
+  }
 }
