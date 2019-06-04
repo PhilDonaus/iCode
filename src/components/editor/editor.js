32b21593d213381d645c01d871f8a7cc6fee91e9
Philippe Donaus <philippe.donaus@gmail.com>
2019-06-03 21:44:16 -0400

    Updtes look of IDE

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -6,7 +6,7 @@ import AceEditor from 'react-ace';
 import 'brace/mode/java';
 import 'brace/mode/html';
 import 'brace/theme/monokai';
- 
+import './editor.css';
 
 export default class Editor extends Component {
   constructor(props){
@@ -14,7 +14,16 @@ export default class Editor extends Component {
     this.onChangeHTML = this.onChangeHTML.bind(this);
     this.onChangeCSS = this.onChangeCSS.bind(this);
     this.onChangeJS = this.onChangeJS.bind(this);
-    this.state = {html:`<p>We Did it!</p>`,
+    this.state = {html:`<!DOCTYPE html>
+<html>
+    <head>
+        <title>I Can Code!</title>
+    </head>
+    <body>
+        <h1>Hi There! I can Code!</h1>
+        <p class="light">See I can do it with a hilighter</p>
+    </body>
+</html>`,
                   css:`body {color:blue}`,
                   js: `function onLoad(editor){ console.log("i've loaded!!");}`}
   }
@@ -36,27 +45,31 @@ export default class Editor extends Component {
   
   render(){
     return(
-      <div>
-        <CodeIDE 
-          mycode={this.state.html}
-          onChange={this.onChangeHTML}
-          mymode={'html'}
-          />
-        <CodeIDE 
-          mycode={this.state.css}
-          onChange={this.onChangeCSS}
-          mymode={'css'}
-          />
-        <CodeIDE 
-          mycode={this.state.js}
-          onChange={this.onChangeJS}
-          mymode={'javascript'}
-          />
-        <RenderDisplay
-          htmlData={this.state.html}
-          cssData={this.state.css}
-          jsData={this.state.js}
-          />
+      <div className="row">
+        <div className="col">
+          <CodeIDE 
+            mycode={this.state.html}
+            onChange={this.onChangeHTML}
+            mymode={'html'}
+            />
+          <CodeIDE 
+            mycode={this.state.css}
+            onChange={this.onChangeCSS}
+            mymode={'css'}
+            />
+          <CodeIDE 
+            mycode={this.state.js}
+            onChange={this.onChangeJS}
+            mymode={'javascript'}
+            />
+        </div>
+        <div className="col">
+          <RenderDisplay
+            htmlData={this.state.html}
+            cssData={this.state.css}
+            jsData={this.state.js}
+            />
+          </div>
       </div>)
   };
 }
@@ -67,24 +80,26 @@ class CodeIDE extends Component {
   	return(
       <div>
           <AceEditor
-              placeholder="Placeholder Text"
-              mode={this.props.mymode}
-              theme="monokai"
-              name="blah2"
-              onLoad={this.props.onLoad}
-              onChange={this.props.onChange}
-              fontSize={14}
-              showPrintMargin={true}
-              showGutter={true}
-              highlightActiveLine={true}
-              value={this.props.mycode}
-              setOptions={{
-              enableBasicAutocompletion: true,
-              enableLiveAutocompletion: true,
-              enableSnippets: false,
-              showLineNumbers: true,
-              tabSize: 4,
-              }}/>
+            placeholder="Placeholder Text"
+            height="100vh"
+            width="100%"
+            mode={this.props.mymode}
+            theme="monokai"
+            name="myeditor"
+            onLoad={this.props.onLoad}
+            onChange={this.props.onChange}
+            fontSize={14}
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine={true}
+            value={this.props.mycode}
+            setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 4,
+            }}/>
       </div>
 	);
   };
@@ -113,7 +128,7 @@ class RenderDisplay extends Component {
   }
   render(){
       return(
-          <div className="col s6">
+          <div className="displayCode">
               <iframe id="displaycase"></iframe>
           </div>
   );
